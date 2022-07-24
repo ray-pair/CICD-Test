85e02800ab0fdeb9d51aa013b5ad2c4fe7f96ae6
UJ15 <dbwogml15@naver.com>
2022-07-25 04:06:09 +0900

    sonar TEST

--- src/main/java/com/example/cicdtest/Testtest.java
+++ src/main/java/com/example/cicdtest/Testtest.java
@@ -2,4 +2,13 @@ package com.example.cicdtest;
 
 public class Testtest {
   int ejtsepgjTEST = 13;
+  int eajijareo = 149214;
+
+  boolean t = true;
+
+  public void ps() {
+    if (t) {
+      int s = 10;
+    }
+  }
 }
